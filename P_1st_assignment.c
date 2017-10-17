e002d15eb34355768833e9a0c0bfd038fc4ed675
2017301006 <31676266+2017301006@users.noreply.github.com>
2017-10-17 15:51:10 +0900

    1st assignment

---
+++ P_1st_assignment.c
@@ -0,0 +1,11 @@
+#incldue <stdio.h>
+
+int main() {
+
+	printf("My student ID No. is 2017301006.\n");
+	printf("My name is ±è³²¿ì.\n");
+	printf("My mobile phone number is 01046032691.\n");
+	printf("My GitHub address is 2017301006@github.com\n");
+
+	return 0;
+}
